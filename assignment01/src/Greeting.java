f54036c15c70ada66c51f96bddc40b9f21f1a886
Joey Huang <joeyh6547@gmail.com>
2023-09-02 19:11:49 -0400

    lab1

--- assignment01/src/Greeting.java
+++ assignment01/src/Greeting.java
@@ -1,9 +1,12 @@
 
 public class Greeting {
     public static void main(String[] args) {
+        // Joey Huang
+        // 08/28/2023
+        // Assignment 1. Greetings
+        // This program simply prints out a greeting to the lab instructor
 
-        System.out.printf("Hello and welcome!");
-
+        System.out.println("Greetings Professor Wenkang");
 
     }
 }
