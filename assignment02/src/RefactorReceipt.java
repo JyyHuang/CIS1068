ff52d6a6709222a9e675afaef720600f38f7bf16
Joey Huang <joeyh6547@gmail.com>
2023-09-11 11:38:52 -0400

    refactor receipt

---
+++ assignment02/src/RefactorReceipt.java
@@ -0,0 +1,23 @@
+public class RefactorReceipt {
+
+    private static double tax(double subtotal, double tax_rate) {
+        return subtotal * tax_rate;
+    }
+
+    private static double tip(double subtotal, double tip_rate) {
+        return subtotal * tip_rate;
+    }
+
+    private static String receipt(double subtotal, double tax, double tip, double total) {
+        return String.format("Subtotal: %s%nTax: %s%nTip: %s%nTotal: %s", subtotal, tax, tip, total);
+    }
+    
+    public static void main(String[] args) {
+        double subtotal = 38 + 40 + 30;
+        double tax = tax(subtotal, 0.08);
+        double tip = tip(subtotal, 0.2);
+        double total = subtotal + tax + tip;
+        System.out.println(receipt(subtotal, tax, tip, total));
+        
+    }
+}
