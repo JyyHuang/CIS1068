868124bb38066a8f4bd02a4c2421b42268720fb0
Joey Huang <joeyh6547@gmail.com>
2023-09-12 12:32:54 -0400

    update

--- assignment01/src/Greeting.java
+++ assignment01/src/Greeting.java
@@ -1,10 +1,10 @@
+// Joey Huang
+// 08/28/2023
+// Assignment 1. Greetings
+// This program simply prints out a greeting to the lab instructor
 
 public class Greeting {
     public static void main(String[] args) {
-        // Joey Huang
-        // 08/28/2023
-        // Assignment 1. Greetings
-        // This program simply prints out a greeting to the lab instructor
 
         System.out.println("Greetings Professor Wenkang");
 
---
+++ assignment03/src/Grades.java
@@ -0,0 +1,16 @@
+/*Joey Huang
+ * CIS1068
+ * Section 003
+ * 09/08/2023
+ * Assignment 2. Get You a Cat
+ * 
+ * This program prints verses from a song called Bought Me a Cat.
+ */
+
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
