d107be14a6e1a89cdb09de17d6d0aadd7bbd5d55
Joey Huang <joeyh6547@gmail.com>
2023-09-17 18:56:16 -0400

    assignment 03

--- LectureNotes/src/App.java
+++ LectureNotes/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String a = new String("hello");
+
+        if (a == "hello") {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
--- assignments/src/assignment03/Grades.java
+++ assignments/src/assignment03/Grades.java
@@ -1,17 +1,33 @@
 /*Joey Huang
  * CIS1068
  * Section 003
- * 09/08/2023
- * Assignment 2. Get You a Cat
+ * 09/17/2023
+ * Assignment 3. Grade Calculator
  * 
- * This program prints verses from a song called Bought Me a Cat.
+ * 
  */
 
 import java.util.Scanner;
 
 public class Grades {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
+    public static int[] gradeWeights() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Homeowork weight? ");
+        int homeWorkWeight = scanner.nextInt();
+        System.out.println("Exam 1 weight? ");
+        int exam1Weight = scanner.nextInt();
+        int exam2Weight = 100 - homeWorkWeight - exam1Weight;
+        System.out.print(String.format("Using weights of %s %s %s", homeWorkWeight, exam1Weight, exam2Weight));
+        scanner.close();
+        int[] weights = {homeWorkWeight, exam1Weight, exam2Weight}
+        return weights;
+    }
+
+    // Homework score
+    public static void homeWorkScore() {
+    
+    }        
+    public static void main(String[] args) {
+        gradeWeights();
     }
 }
