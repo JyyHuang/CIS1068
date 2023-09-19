3a2387bd9c209ac8f60f2c944a24ca8f6a8b07c6
Joey Huang <joeyh6547@gmail.com>
2023-09-19 19:49:46 -0400

    lab3

--- assignments/src/assignment03/Grades.java
+++ assignments/src/assignment03/Grades.java
@@ -1,26 +1,39 @@
 /*Joey Huang
  * CIS1068
  * Section 003
- * 09/17/2023
+ * 09/19/2023
  * Assignment 3. Grade Calculator
  * 
- * 
+ * This program asks the user for inputs of grade weights, assignments, and exam scores to calculate the final grade of the semester
  */
 
 import java.util.Scanner;
 
 public class Grades {
 
+    // Exam Score
+    public static double examGrade(int weight, int score, int curve) {
+
+        double examGrade = score + curve;
+
+        // Checks edge cases for negative and if greater than total
+        if (score < 0) {
+            examGrade = 0;
+        } else if (score > 100 || examGrade > 100) {
+            examGrade = 100;
+        }
+
+        double weightedGrade = (examGrade / 100) * weight;
+        System.out.println(String.format("Total points = %s / %s", examGrade, 100));
+;       
+        return weightedGrade;
+    }
+
     // Homework score
     public static double homeWorkGrade(int weight, int numberOfAssignments, double avg, int lateDays, int labs) {
-        // homework 10 points
-        // lab 4 points
-        //num hw and labs is ==
-        //more late days than half than reduce 10% 
-        // no late days then 5 points extra credit
-        // cant get more than 100% of grade
-        double hwGrade;
-
+        
+        // Checks edge cases if number of assignments is 0 or negative
+        // Also caps the max points and checks if avg is negative
         if (numberOfAssignments <= 0) {
             return weight;
         } else if (avg < 0) {
@@ -29,21 +42,34 @@ public class Grades {
             avg = 10;
         }
 
-        int homeworkTotal = (numberOfAssignments * 10) + (((labs*4/numberOfAssignments))*10);
-        hwGrade = (avg * numberOfAssignments);
-        
-        if (lateDays > (numberOfAssignments / 2))
-        {
+
+        int assignmentGrade = numberOfAssignments * 10;
+
+        // Assignment grade + lab grade
+        int homeworkTotal = (assignmentGrade) + ((numberOfAssignments) * 4);
+
+        double hwGrade = (avg * numberOfAssignments);
+
+        // Checks late day requirements
+        if (lateDays > (numberOfAssignments / 2)) {
             hwGrade = hwGrade * .90;
-        } else if (lateDays == 0 && hwGrade < numberOfAssignments * 10) {
-            hwGrade = hwGrade + 5;
+        } else if (lateDays == 0 && (hwGrade < assignmentGrade)) {
+            if ((hwGrade + 5) > assignmentGrade) {
+                hwGrade = assignmentGrade;
+            } else {
+                hwGrade = hwGrade + 5;
+            }
         }
-        
+
         double total = hwGrade + labs * 4;
-        System.out.println(String.format("Total points = %s / %s", total,homeworkTotal));
         double weightedGrade = (total / homeworkTotal) * weight;
+        System.out.println(String.format("Total points = %s / %s", total, homeworkTotal));
         return weightedGrade;
-    }        
+    }
+            
+    public static void printWeightedScore(double score) {
+        System.out.println(String.format("Weighted score = %.2f", score));
+    }
 
     public static void main(String[] args) {
         // Get weights
@@ -55,8 +81,10 @@ public class Grades {
 
         int exam1Weight = scanner.nextInt();
         int exam2Weight = 100 - homeWorkWeight - exam1Weight;
+
         System.out.println(String.format("Using weights of %s %s %s", homeWorkWeight, exam1Weight, exam2Weight));
 
+        // Weighted Homework
         System.out.println("Homework:");
         System.out.println("Number of assignments?");
         int assignmentNumber = scanner.nextInt();
@@ -70,16 +98,33 @@ public class Grades {
         System.out.println("Labs attended?");
         int numLabs = scanner.nextInt();
 
+        
         double weightedHomework = homeWorkGrade(homeWorkWeight, assignmentNumber, avgGrade, lateDays, numLabs);
-        System.out.println(String.format("Weighted score = %.2f",weightedHomework));
+        printWeightedScore(weightedHomework);
 
+        //Exam 1
+        System.out.println("Exam 1:");
+        System.out.println("Score?");
+        int exam1Score = scanner.nextInt();
 
+        System.out.println("Curve?");
+        int exam1Curve = scanner.nextInt();
 
+        double weightedExam1 = examGrade(exam1Weight, exam1Score, exam1Curve);
+        printWeightedScore(weightedExam1);
 
+        //Exam 2
+        System.out.println("Exam 2:");
+        System.out.println("Score?");
+        int exam2Score = scanner.nextInt();
+        System.out.println("Curve?");
+        int exam2Curve = scanner.nextInt();
+        double weightedExam2 = examGrade(exam2Weight, exam2Score, exam2Curve);
+        printWeightedScore(weightedExam2);
 
+        double courseGrade = weightedHomework + weightedExam1 + weightedExam2;
+        System.out.print(String.format("Course grade = %.2f", courseGrade));
 
         scanner.close();
-
-
     }
 }
