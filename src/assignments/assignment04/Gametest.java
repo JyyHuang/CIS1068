505a16591e5f29630d15cda5ce6cacfc2112fd1f
Joey Huang <joeyh6547@gmail.com>
2023-09-28 21:39:24 -0400

    test cases

--- src/assignments/assignment04/Gametest.java
+++ src/assignments/assignment04/Gametest.java
@@ -3,9 +3,23 @@ package assignments.assignment04;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class Gametest {
+public class GameTest {
+
     @Test
-    public void test(){
-        assertEquals(2, 1+1);
+    public void testWinner(){
+
+        assertEquals(true, Game.checkWinner("melltorp", "utespelare"));
+        assertEquals(true, Game.checkWinner("melltorp", "olserod"));
+        assertEquals(true, Game.checkWinner("utespelare", "knarrevik"));
+        assertEquals(true, Game.checkWinner("knarrevik", "melltorp"));
+        assertEquals(true, Game.checkWinner("olserod", "yngvar"));
+        assertEquals(true, Game.checkWinner("yngvar", "melltorp"));
+        assertEquals(false, Game.checkWinner("melltorp", "melltorp"));
+        assertEquals(false, Game.checkWinner("utespleare", "melltorp"));
+        assertEquals(false, Game.checkWinner("olserod", "melltorp"));
+        assertEquals(false, Game.checkWinner("knarrevik", "utespelare"));
+        assertEquals(false, Game.checkWinner("yngvar", "olserod"));
+        assertEquals(false, Game.checkWinner("yngvar", "olserod"));
+
     }
 }
