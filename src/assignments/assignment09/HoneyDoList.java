c10a4f667d521b8d55ed6ef4b9b88504da2a0e7e
Joey Huang <joeyh6547@gmail.com>
2023-11-08 13:32:50 -0500

    honeydolist

--- src/assignments/assignment09/HoneyDoList.java
+++ src/assignments/assignment09/HoneyDoList.java
@@ -3,9 +3,10 @@ package assignments.assignment09;
 public class HoneyDoList {
     private Task[] tasks;
     private int numTasks;
-    private static final int INITIAL_CAPACITY;
+    private static final int INITIAL_CAPACITY = 10;
 
     public HoneyDoList(){
-        
+        this.numTasks = 0;
+        this.tasks = new Task[INITIAL_CAPACITY];
     }
 }
